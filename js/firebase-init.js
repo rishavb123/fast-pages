953ccc9a5ea9663c04b18db60b399d327c898f19
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-01-15 09:56:19 -0500

    initializing firestore

---
+++ js/firebase-init.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDSFaKe74_aj3krf7_HtQgqTZGRZGoQ30o",
+    authDomain: "fast-pages-103f8.firebaseapp.com",
+    projectId: "fast-pages-103f8",
+    storageBucket: "fast-pages-103f8.appspot.com",
+    messagingSenderId: "65010622496",
+    appId: "1:65010622496:web:82e911eeed53b695975c45"
+};
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
